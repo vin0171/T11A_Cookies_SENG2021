5fc79f68d8fa474e41eafa6b1a2db7ef336c5a28
Ashwin <ashwintheagarajan@gmail.com>
2025-03-06 16:18:51 +1100

    added stub functions and stub apis, updated data.js to prototype, and fixed swagger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,8 +6,11 @@ import config from './config.json';
 import YAML from 'yaml';
 import sui from 'swagger-ui-express';
 import fs from 'fs';
-import path from 'path';
+import path, { parse } from 'path';
 import process from 'process';
+import * as invoices from './invoices';
+import * as companies from './companies';
+import * as users from './users';
 
 // Set up web app
 const app = express();
@@ -40,6 +43,110 @@ app.listen(PORT, HOST, () => {
     console.log(`Example app listening at ${url}`);
     console.log('Control+C to quit the app')
 });
+
+
+// ========================================================================= //
+// Iteration 1 
+// ========================================================================= //
+
+app.post('/v1/user/register', (req: Request, res: Response) => {
+  const { email, password, nameFirst, nameLast } = req.body;
+  const response = users.registerUser(email, password, nameFirst, nameLast);
+
+  res.json("Not Implemented");
+});
+
+app.post('/v1/auth/login', (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const response = users.authLogin(email, password);
+
+  res.json("Not Implemented");
+});
+
+
+app.post('/v1/auth/logout', (req: Request, res: Response) => {
+  const { token } = req.body;
+  const response = users.authLogout(token);
+
+  res.json("Not Implemented");
+});
+
+
+app.post('/v1/company/register', (req: Request, res: Response) => {
+  const { companyName, companyAbn, adminEmail, adminPassword, contactNumber } = req.body;
+  const response = companies.registerCompany(companyName, companyAbn, adminEmail, adminPassword, contactNumber);
+
+  res.json("Not Implemented");
+});
+
+
+app.put('/v1/company/user/add', (req: Request, res: Response) => {
+  const { email } = req.body;
+  const response = companies.addCompanyUser(email);
+
+  res.json("Not Implemented");
+});
+
+
+app.post('/v1/invoice', (req: Request, res: Response) => {
+  // change whatever
+  const { sender, receiver, issueDate, dueDate } = req.body;
+  const response = invoices.createInvoice();
+
+  res.json("Not Implemented");
+});
+
+
+app.get('/v1/invoice/:invoiceId', (req: Request, res: Response) => {
+  // change whatever
+  const { sender, receiver, issueDate, dueDate } = req.body;
+  const response = invoices.getInvoice();
+
+  res.json("Not Implemented");
+});
+
+
+app.put('/v1/invoice/:invoiceId/edit/details', (req: Request, res: Response) => {
+  // change whatever
+  const { sender, receiver, issueDate, dueDate } = req.body;
+  const response = invoices.editInvoiceDetails(token);
+
+  res.json("Not Implemented");
+});
+
+app.put('/v1/invoice/:invoiceId/edit/status', (req: Request, res: Response) => {
+  // change whatever
+  const { token, status } = req.body;
+  const response = invoices.editInvoiceStatus(token, status);
+
+  res.json("Not Implemented");
+});
+
+// CHECK HAS TO be in Trash
+app.delete('/v1/invoice/:invoiceId', (req: Request, res: Response) => {
+  // change whatever
+  const invoiceId = parseInt(req.params.invoiceId);
+  const { token } = req.body;
+  const response = invoices.deleteInvoice(token, invoiceId);
+
+  res.json("Not Implemented");
+});
+
+
+app.get('/v1/invoice/list', (req: Request, res: Response) => {
+  // change whatever
+  const { sender, receiver, issueDate, dueDate } = req.body;
+  const response = listCompanyInvoices();
+
+  res.json("Not Implemented");
+});
+
+
+
+
+
+
+
   
 // The thing below causes memeory leaks for me lol
 // // For coverage, handle Ctrl+C gracefully 
